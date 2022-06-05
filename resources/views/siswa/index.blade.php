362ae5592d038017d6b0d8f907e6db51091ac6bb
rizalgemilangputra <rizal.gemilangputra@gmail.com>
2022-06-06 00:23:01 +0700

    add ui siswa modul

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -1,7 +1,70 @@
 @extends('layouts.app')
 
-@section('title') Data Siswa @endsection
+@section('title')
+    Data Siswa
+@endsection
 
 @section('content')
-    asdasd
+    <section class="section">
+        <div class="row" id="table-striped">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#modal-input">
+                            Tambah Siswa
+                        </button>
+                    </div>
+                    <div class="card-content px-4 pb-4">
+                        <!-- table striped -->
+                        <div class="table-responsive">
+                            <table class="table table-striped mb-0">
+                                <thead>
+                                    <tr>
+                                        <th>No. Induk</th>
+                                        <th>Nama</th>
+                                        <th>Rerata Nilai</th>
+                                        <th>Absensi</th>
+                                        <th>Sikap</th>
+                                        <th>Nilai Extra</th>
+                                        <th>Opsi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="text-bold-500">10114024</td>
+                                        <td>Rizal Gemilang Putra</td>
+                                        <td>20</td>
+                                        <td>A</td>
+                                        <td>B</td>
+                                        <td>C</td>
+                                        <td>
+                                            <a href="#">
+                                                <i class="badge-circle badge-circle-light-secondary font-medium-1"
+                                                    data-feather="mail">1</i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-bold-500">10114000</td>
+                                        <td>Revina Nurjanah</td>
+                                        <td>20</td>
+                                        <td>A</td>
+                                        <td>B</td>
+                                        <td>C</td>
+                                        <td>
+                                            <a href="#">
+                                                <i class="badge-circle badge-circle-light-secondary font-medium-1"
+                                                    data-feather="mail">1</i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    @include('siswa.modal-input')
 @endsection
---
+++ resources/views/siswa/modal-input.blade.php
@@ -0,0 +1,49 @@
+<div class="modal fade" id="modal-input" tabindex="-1" aria-labelledby="modal-input-label" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modal-input-label">Input Data Siswa</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="" class="p-4">
+                    <div class="form-group">
+                        <label for="no_induk">No. Induk</label>
+                        <input type="number" class="form-control" id="no_induk" placeholder="No. Induk" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="nama">Nama</label>
+                        <input type="text" class="form-control" id="nama" placeholder="Nama" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="rerata_nilai">Rerata Nilai</label>
+                        <input type="number" class="form-control" id="rerata_nilai" placeholder="Rerata Nilai"
+                            required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="nilai_absen">Nilai Absen</label>
+                        <input type="number" class="form-control" id="nilai_absen" placeholder="Nilai Absen" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="nilai_sikap">Nilai Sikap</label>
+                        <input type="number" class="form-control" id="nilai_sikap" placeholder="Nilai Sikap" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="nilai_extrakulikuler">Nilai Extrakulikuler</label>
+                        <input type="number" class="form-control" id="nilai_extrakulikuler"
+                            placeholder="Nilai Extrakulikuler" required>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                <button type="button" class="btn btn-success">Simpan</button>
+              </div>
+        </div>
+    </div>
+</div>

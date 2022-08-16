9e6b72cf74f80faeb7e6a796c801de41a3786efe
rizalgemilangputra <rizal.gemilangputra@gmail.com>
2022-08-17 00:33:28 +0700

    fixing generate laporan

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -16,11 +16,15 @@ class LaporanController extends Controller
             if ($request->jenis == 'tahun_ajaran') {
                 $reports = Laporan::join('detail_laporan', 'laporan.id', '=' , 'detail_laporan.id_laporan')
                     ->join('siswa', 'detail_laporan.id_siswa', '=' , 'siswa.id')
-                    ->join('detail_siswa', 'siswa.id', 'detail_siswa.id_siswa')
+                    ->join('detail_siswa', function($sql){
+                        $sql->on('siswa.id', 'detail_siswa.id_siswa');
+                        $sql->on('detail_siswa.kelas', 'detail_laporan.kelas');
+                    })
                     ->where('laporan.jenis', $request->jenis)
                     ->where('laporan.tahun_ajaran', $request->tahun_ajaran)
                     ->orderBy('detail_laporan.rank', 'ASC')
                     ->paginate(10)->appends(['jenis' => $request->jenis, 'tahun_ajaran' => $request->tahun_ajaran, 'kelas' => $request->kelas]);
+
             } else if ($request->jenis == 'kelas') {
                 $reports = Laporan::join('detail_laporan', 'laporan.id', '=' , 'detail_laporan.id_laporan')
                     ->join('siswa', 'detail_laporan.id_siswa', '=' , 'siswa.id')
